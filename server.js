11e7966bb393009101f69e7413dbc59c6689e393
Nikolaevich <SWAT-Fox@mail.ru>
2026-04-19 21:51:45 +0300

    54

--- server.js
+++ server.js
@@ -3,6 +3,30 @@ const express = require('express');
 
 const app = express();
 
+// CORS: разрешаем только твой GitHub‑сайт
+app.use((req, res, next) => {
+  const allowedOrigin = 'https://swat92shtorm.github.io';
+  const origin = req.headers.origin;
+
+  if (origin === allowedOrigin) {
+    res.header('Access-Control-Allow-Origin', origin);
+  }
+
+  // Разрешаем нужные методы и заголовки
+  res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Content-Type');
+
+  // Обрабатываем preflight (OPTIONS)
+  if (req.method === 'OPTIONS') {
+    return res.status(200).end();
+  }
+
+  next();
+});
+
+// Тело приложения
+app.use(express.json());
+
 app.get('/', (req, res) => {
   res.send('Server works!');
 });

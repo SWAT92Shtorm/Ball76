329c8db718478159aaac74e755589b7f822d7dc0
Nikolaevich <SWAT-Fox@mail.ru>
2026-08-25 22:17:32 +0300

    ок

--- server.js
+++ server.js
@@ -51,7 +51,7 @@ app.use((req, res, next) => {
     res.header('Access-Control-Allow-Origin', origin);
   }
   res.header('Access-Control-Allow-Methods', 'GET, POST, PATCH, DELETE, OPTIONS');
-  res.header('Access-Control-Allow-Headers', 'Content-Type');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, bypass-tunnel-reminder');
   if (req.method === 'OPTIONS') {
     return res.status(200).end();
   }
